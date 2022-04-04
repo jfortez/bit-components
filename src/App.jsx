7e07d13818ff1045d33a136be51c094c0b54bc65
Jean Caiza <jcaiza@bsoftsolution.net>
2022-04-04 12:06:30 -0500

    commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Component } from "@bsoft-solution/pruebas.ui.button";
+const App = () => {
+  return <Component text="Hello from Vercel" />;
+};
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteCommonjs, esbuildCommonjs } from "@originjs/vite-plugin-commonjs";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    // This creates part of the magic.
+    viteCommonjs(),
+
+    // https://www.npmjs.com/package/@vitejs/plugin-react
+    react({
+      // exclude: /\.stories\.(t|j)sx?$/,
+
+      babel: {
+        // presets: ['@babel/preset-env'],
+        // babelrc: true,
+        parserOpts: {
+          plugins: ["optionalChaining", "nullishCoalescingOperator", "logicalAssignment"],
+        },
+      },
+    }),
+  ],
+
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        // Solves:
+        // https://github.com/vitejs/vite/issues/5308
+        // add the name of your package
+        esbuildCommonjs(["tiny-slider", "tiny-slider-react"]),
+      ],
+    },
+  },
+});
